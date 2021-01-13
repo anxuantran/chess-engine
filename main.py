ec9d67b395f3fd8f6e62b6ac20084e3974172796
An Tran <antran@Ans-MacBook-Pro.fios-router.home>
2021-01-13 18:11:42 -0500

    ver 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import chess
+
 def pos_eval(board,color):
     
     piece_val = {
@@ -36,7 +38,19 @@ def pos_eval(board,color):
                     max_min_move = move
     
     return(max_min_move)
-	
-	
-	
+
+def board_score(board):
+    score = 0
+
+    piece_val = { 1: 10, 2:30, 3: 30, 4: 50, 5: 90, 6: 900}
+    piece_map = board.piece_map()
+    for i, piece in piece_map.items():
+        if piece.color == chess.WHITE:
+            score += piece_val[piece.piece_type]
+        else:
+            score -= piece_val[piece.piece_type]
+    return score
+
+board = chess.Board()
+print(board_score(board))
 
